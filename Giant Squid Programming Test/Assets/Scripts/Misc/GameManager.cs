ef58f90debeb4341db6519626c44141b03c1492a
DESKTOP-U2RKT7R\Daggett <andrewdecker3526@gmail>
2017-03-28 18:43:15 -0400

    Finished Gameplay Test
    Uploading the completed gameplay test for review

---
+++ Giant Squid Programming Test/Assets/Scripts/Misc/GameManager.cs
@@ -0,0 +1,21 @@
+﻿/* Script allowing for the player to reload the level is anything breaks
+ * and quit the game
+ * */
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private void Update()
+    {
+        if(Input.GetButtonDown("Reset"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        if(Input.GetButtonDown("Quit"))
+        {
+            Application.Quit();
+        }
+    }
+}
